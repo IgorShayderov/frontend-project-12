d9960246c02cfa316cb2ed240c52c6dc37b79501
Igor <flashmob123@gmail.com>
2022-12-18 20:07:47 +0200

    Form validation

--- src/routes/login-page.jsx
+++ src/routes/login-page.jsx
@@ -1,14 +1,18 @@
 import '../style/login-page.css';
 
 import React from 'react';
+import {
+  Formik, Form, Field,
+} from 'formik';
 import { useRouteError } from 'react-router-dom';
+import * as Yup from 'yup';
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   const handleSubmit = (event) => {
-    event.preventDefault();
+    console.info(event, 'submiting...');
   };
 
   return (
@@ -16,25 +20,53 @@ export default function ErrorPage() {
       <article>
         <h1 className="login__title">Login</h1>
 
-        <form className="login__form" onSubmit={handleSubmit}>
-          <label htmlFor="" className="login__form-label">
-            <input
-              placeholder="Login"
-              type="text"
-              className="login__form-input" />
-          </label>
+        <Formik
+          initialValues={{
+            login: '',
+            password: '',
+          }}
+          validationSchema={Yup.object({
+            login: Yup.string()
+              .max(15, 'Must be 15 characters or less')
+              .min(3, 'Must be at least 3 characters')
+              .required('Required'),
+            password: Yup.string()
+              .max(15, 'Must be 15 characters or less')
+              .min(5, 'Must be at least 5 characters')
+              .required('Required'),
+          })}
+          onSubmit={handleSubmit}
+        >
+          {({ errors, touched }) => (
+            <Form className="login__form">
+              <label htmlFor="login" className="login__form-label">
+                <Field
+                  id="login"
+                  name="login"
+                  className="login__form-input"
+                  placeholder="Login"
+                  type="text" />
+              </label>
 
-          <label htmlFor="" className="login__form-label">
-            <input
-              placeholder="Password"
-              type="password"
-              className="login__form-input" />
-          </label>
+              { errors.login && touched.login ? <div>{ errors.login }</div> : null }
 
-          <button type="submit">
+              <label htmlFor="password" className="login__form-label">
+                <Field
+                  id="password"
+                  name="password"
+                  className="login__form-input"
+                  placeholder="Password"
+                  type="password" />
+              </label>
+
+              { errors.password && touched.password ? <div>{ errors.password }</div> : null }
+
+              <button type="submit">
               Log in
-          </button>
-        </form>
+              </button>
+            </Form>
+          )}
+        </Formik>
       </article>
     </div>
   );
