2e479d285e439149487cabdd2ee8ca00bc00a2df
Igor <flashmob123@gmail.com>
2022-12-31 15:46:26 +0200

    Authentication form styled

--- src/routes/login-page.jsx
+++ src/routes/login-page.jsx
@@ -1,17 +1,24 @@
 import '../style/login-page.scss';
 
-import React from 'react';
-import { FormGroup } from 'react-bootstrap';
+import React, { useEffect, useRef } from 'react';
+import { Button, FormGroup, FormLabel } from 'react-bootstrap';
 import {
   Formik, Form, Field, ErrorMessage,
 } from 'formik';
-import { useRouteError } from 'react-router-dom';
+import { useRouteError, useLocation } from 'react-router-dom';
 import * as Yup from 'yup';
 import axios from 'axios';
 
-export default function ErrorPage() {
+const LoginPage = () => {
   const error = useRouteError();
   console.error(error);
+
+  const location = useLocation;
+  const loginInput = useRef(null);
+
+  useEffect(() => {
+    loginInput.current.focus();
+  }, [location]);
 
   const handleSubmit = ({ login, password }, { resetForm }) => {
     axios.post('login', {
@@ -22,8 +29,8 @@ export default function ErrorPage() {
   };
 
   return (
-    <div className="login">
-      <article>
+    <div className="d-flex justify-content-center align-items-center h-100">
+      <article className="login border border-primary rounded-2 p-3">
         <h1 className="login__title">Login</h1>
 
         <Formik
@@ -44,42 +51,51 @@ export default function ErrorPage() {
           onSubmit={handleSubmit}
         >
           {() => (
-            <Form className="login__form">
-              <FormGroup>
-                <label htmlFor="login" className="login__form-label">
+            <Form className="">
+              <FormGroup className="mb-2">
+                <FormLabel htmlFor="login" className="w-100">
                   <Field
                     id="login"
+                    innerRef={loginInput}
                     autoComplete="username"
                     name="login"
-                    className="login__form-input"
+                    className="w-100"
                     placeholder="Login"
+                    aria-describedby="loginErrorMessage"
                     type="text" />
-                </label>
+                </FormLabel>
 
-                <ErrorMessage name="login"/>
+                <p className="error-message m-0">
+                  <ErrorMessage id="loginErrorMessage" name="login"/>
+                </p>
               </FormGroup>
 
-              <FormGroup>
-                <label htmlFor="password" className="login__form-label">
+              <FormGroup className="mb-2">
+                <FormLabel htmlFor="password" className="w-100">
                   <Field
                     id="password"
                     autoComplete="current-password"
                     name="password"
-                    className="login__form-input"
+                    className="w-100"
                     placeholder="Password"
+                    aria-describedby="passwordErrorMessage"
                     type="password" />
-                </label>
+                </FormLabel>
 
-                <ErrorMessage name="password"/>
+                <p className="error-message m-0">
+                  <ErrorMessage id="passwordErrorMessage" name="password"/>
+                </p>
               </FormGroup>
 
-              <button type="submit">
+              <Button type="submit">
               Log in
-              </button>
+              </Button>
             </Form>
           )}
         </Formik>
       </article>
     </div>
   );
-}
+};
+
+export default LoginPage;
