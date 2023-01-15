bdb8592de6bb67c2772af2719eb7062dfe064e8c
Igor <flashmob123@gmail.com>
2023-01-15 14:31:53 +0200

    fix: Make app init asynchronous

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import axios from 'axios';
 import { Provider } from 'react-redux';
+import { I18nextProvider } from 'react-i18next';
 
 import Root from './routes/root.jsx';
 import ErrorPage from './routes/error-page.jsx';
@@ -13,6 +14,7 @@ import SignUpPage from './routes/signup-page.jsx';
 import store from './slices/index.js';
 import AuthProvider from './components/auth-provider.jsx';
 import Header from './components/header.jsx';
+import i18n from './i18n.js';
 
 axios.defaults.baseURL = 'api/v1';
 
@@ -34,15 +36,22 @@ const router = createBrowserRouter([
   },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <AuthProvider>
-        <Header />
+const initApp = async () => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
 
-        <RouterProvider router={router} />
-      </AuthProvider>
-    </Provider>
-  </React.StrictMode>,
-);
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <I18nextProvider i18n={i18n}>
+          <AuthProvider>
+            <Header />
+
+            <RouterProvider router={router} />
+          </AuthProvider>
+        </I18nextProvider>
+      </Provider>
+    </React.StrictMode>,
+  );
+};
+
+initApp();
