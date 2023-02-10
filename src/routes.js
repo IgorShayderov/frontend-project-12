c333d38f221b3126e687d789b32e00ee1b7e0612
Igor <flashmob123@gmail.com>
2023-02-10 13:10:38 +0200

    fix: Separate routes

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+export default {
+  api: {
+    loginPath: () => '/login',
+    signUpPath: () => '/signup',
+    dataPath: () => '/data',
+  },
+  rootPath: () => '/',
+  loginPath: () => '/login',
+  signUpPath: () => '/signup',
+};
