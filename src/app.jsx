26e6ff45ed062be8a1bca9a29087fc046569901c
Igor <flashmob123@gmail.com>
2023-03-03 15:22:04 +0200

    feat: Separate App logic

---
+++ src/app.jsx
@@ -0,0 +1,39 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+  Outlet,
+} from 'react-router-dom';
+
+import Root from './routes/root.jsx';
+import ErrorPage from './routes/error-page.jsx';
+import LoginPage from './routes/login-page.jsx';
+import SignUpPage from './routes/signup-page.jsx';
+import Header from './components/header.jsx';
+
+import routes from './routes';
+import { useAuth } from './components/auth-provider.jsx';
+
+const PrivateOutlet = () => {
+  const auth = useAuth();
+
+  return auth.currentUser ? <Outlet /> : <Navigate to={routes.loginPath()} />;
+};
+
+const App = () => (
+  <Router>
+    <Header />
+
+    <Routes>
+      <Route path={routes.loginPath()} element={<LoginPage />} />
+      <Route path={routes.signUpPath()} element={<SignUpPage />} />
+      <Route path={routes.rootPath()} element={<PrivateOutlet />}>
+        <Route path="" element={<Root />} />
+      </Route>
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+  </Router>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,46 +2,23 @@ import './index.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import { Provider } from 'react-redux';
 import { I18nextProvider } from 'react-i18next';
 import * as Yup from 'yup';
 import leoProfanity from 'leo-profanity';
 
-import Root from './routes/root.jsx';
-import ErrorPage from './routes/error-page.jsx';
-import LoginPage from './routes/login-page.jsx';
-import SignUpPage from './routes/signup-page.jsx';
 import AuthProvider from './components/auth-provider.jsx';
 import ToastProvider from './components/toast-provider.jsx';
 import RollbackProvider from './components/rollback-provider.jsx';
-import Header from './components/header.jsx';
+import App from './app.jsx';
 
 import store from './slices/index.js';
 import initI18n from './i18n.js';
-import routes from './routes';
 import getYupLocale from './locales/getYupLocale.js';
 import api from './api';
 import { actions as messagesActions } from './slices/messages-slice';
 import { actions as channelsActions } from './slices/channels-slice';
-
-const router = createBrowserRouter([
-  {
-    path: routes.rootPath(),
-    element: <Root />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: routes.loginPath(),
-    element: <LoginPage />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: routes.signUpPath(),
-    element: <SignUpPage />,
-    errorElement: <ErrorPage />,
-  },
-]);
 
 const initApp = async () => {
   const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -76,9 +53,7 @@ const initApp = async () => {
           <I18nextProvider i18n={i18n}>
             <AuthProvider>
               <ToastProvider>
-                <Header />
-
-                <RouterProvider router={router} />
+                <App />
               </ToastProvider>
             </AuthProvider>
           </I18nextProvider>
